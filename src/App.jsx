f0ade336f5c24a1227dd8da050e28aa77e25ec7a
Rahul Parmar <rahulparmar636790@gmail.com>
2024-10-18 19:19:35 +0530

    Created Google Gemini clone with React and Gemini API

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./componenets/sidebar/sidebar";
+import Main from "./componenets/Main/Main";
+
+const App = () =>{
+  return (
+    <>
+    <Sidebar/>
+    <Main/>
+    </>
+  )
+}
+
+export default App 
